9cabdadd19e7cba50ff26229f1274f7572686575
kevinhui98 <kevin.hui@envirostudies.org>
2017-08-16 15:15:55 +0000

    second commit

--- homePage.js
+++ homePage.js
@@ -1,12 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$("#tradeWeb").click(function(){
-	$(".twMarket").show();
-	$(".bbgMarket").hide();
-	alert("You are in trade web!")
-});
-$("#bloomberg").click(function(){
-	$(".bbgMarket").show();
-	$(".twMarket").hide(); 
-});
+	var term;
+	var market;
+
+	$("#tradeWeb").click(function() {
+		$("#dropdownMenuButton").text("TW UST US RTS");
+		// alert($("#tradeWeb").text());
+		// $(".twMarket").show();
+		// $(".bbgMarket").hide();
+	});
+	$("#bloomberg").click(function() {
+		$("#dropdownMenuButton").text("BBG IRS US RTS");
+		// $(".bbgMarket").show();
+		// $(".twMarket").hide();
+	});
+	
+	
+	$("#searchBtn").click(function() {
+		term = $("#term").val()
+		market = $("#dropdownMenuButton").text();
+		if (term === "TRSY_20170809_1305" && market === "TW UST US RTS") {
+			$("#nicePage1305").show();
+			$("nicePage344").hide();
+		} else if(term === "TRSY_20170809_344" && market === "TW UST US RTS"){
+			$("#nicePage344").show();
+			$("#nicePage1305").hide();
+		} else if(term === "00000323_20170809_07" && market === "BBG IRS US RTS"){
+			$("#nicePage323").show();
+			$("#nicePage487").hide();
+		} else if(term === "00000487_20170809_07" && market === "BBG IRS US RTS"){
+			$("#nicePage487").show();
+			$("#nicePage323").hide();
+		}
+
+	});
+	$("#moreInfo").click(function() {
+	    $("#extraInfo").show();
+	});
 });
